4b32701e4b40e8540c4781946f15fad73e68b3e9
Kunal Ghosh <kunal.t2@gmail.com>
2016-11-27 21:04:26 +0200

    A few refactors, but important ones are (1) To subtract -1 from the test_idxs and train_idxs since they are starting from 1. (2) these two vectors need to be cast to integer so that we can use them to index into vector. (3) Notice the indexing scheme used to create ktrain and ktest_train. Simply indexing as Kernel[test_idxs, train_idxs] would not work.

--- Assignment6/src/classify_and_predict.py
+++ Assignment6/src/classify_and_predict.py
@@ -1,3 +1,4 @@
+import pdb
 import numpy as np
 from sklearn import svm
 from get_alignment import get_alignment
@@ -5,36 +6,50 @@ from get_alignment import get_alignment
 if __name__ == '__main__':
 
     # Load data
+    path_prefix ='/Users/kunal/Workspace/datasets/KernelMethods/Assignment6/data' 
 
-    kernel_files = ['~/Workspace/datasets/KernelMethods/Assignment6/data/K_exp.txt',
-                    '~/Workspace/datasets/KernelMethods/Assignment6/data/K_mgi.txt',
-                    '~/Workspace/datasets/KernelMethods/Assignment6/data/K_mpi.txt',
-                    '~/Workspace/datasets/KernelMethods/Assignment6/data/K_pfamdom.txt',
-                    '~/Workspace/datasets/KernelMethods/Assignment6/data/K_sw.txt',
-                    '~/Workspace/datasets/KernelMethods/Assignment6/data/K_tap.txt']
+    kernel_files = [path_prefix + '/K_exp.txt',
+                    path_prefix + '/K_mgi.txt',
+                    path_prefix + '/K_mpi.txt',
+                    path_prefix + '/K_pfamdom.txt',
+                    path_prefix + '/K_sw.txt',
+                    path_prefix + '/K_tap.txt']
 
+    print("Loading data...")
     Kernels    = [np.loadtxt(file) for file in kernel_files]
-    Y          = np.loadtxt('~/Workspace/datasets/KernelMethods/Assignment6/data/y.txt')
-    test_idxs  = np.loadtxt('~/Workspace/datasets/KernelMethods/Assignment6/data/test_set.txt')
-    train_idxs = np.loadtxt('~/Workspace/datasets/KernelMethods/Assignment6/data/train_set.txt')
+    Y          = np.loadtxt(path_prefix + '/y.txt').astype(np.int32)
+    test_idxs  = np.loadtxt(path_prefix + '/test_set.txt').astype(np.int32)-1
+    train_idxs = np.loadtxt(path_prefix + '/train_set.txt').astype(np.int32)-1
+    print("Data Loaded...")
 
     # get the linear combination weights
     d = get_alignment(Kernels, Y) 
+    print("Got the linear combination weights...")
 
     # calculate the ALIGNF kernel
     alignf = np.zeros_like(Kernels[0])
-    for idx,kernel in enumerate(Kernels)
-        np.sum(alignf, kernel * d[idx], out=alignf)
+    for idx,kernel in enumerate(Kernels):
+        # np.sum(alignf, kernel * d[idx], out=alignf)
+        alignf += kernel * d[idx]
+    print("Calculated the ALIGNF kernel...")
 
     kernel_dict = {'ALIGNF':alignf}
 
     for kernel_filename, kernel in zip(kernel_files, Kernels):
-        kernel_name = kernel_filename.split(".")[0].split("/")[2]
+        kernel_name = kernel_filename.split(".")[0].split("/")[-1]
         kernel_dict[kernel_name] = kernel
-    
-    # For each of the 7 Kernels
-        # train 13 one-vs-all svm classifiers
-        # Calculate the classification accuracy 
 
-        
-    
+    Accuracies = {}
+    for name, kernel in kernel_dict.items():
+        Accuracies[name] = []
+        # For each of the 7 Kernels
+        ktrain = kernel[train_idxs,:][:,train_idxs]
+        ktest_train = kernel[test_idxs,:][:,train_idxs] 
+        for col_idx in range(Y.shape[1]):
+           model = svm.SVC(C=1, kernel='precomputed')
+           model.fit(ktrain, Y[train_idxs,col_idx])
+           y_predict = model.predict(ktest_train)
+           Accuracies[name].append(np.mean(y_predict == Y[test_idxs,col_idx]))
+           # train 13 one-vs-all svm classifiers
+           # Calculate the classification accuracy 
+        print("{} : {} ".format(name, Accuracies[name]))
